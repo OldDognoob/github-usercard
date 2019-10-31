6bc51a783bfbbd5f678bd2d90b48c7e578a3220d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-10-31 21:08:31 +0200

    step 3 initial changes

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,13 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/OldDognoob
 */
-axios.get('https://api.github.com/users/OldDognoob')
-.then(response => {
-  console.log(response);
-})
-.catch(error => {
-  console.log('error');
-});
+axios
+  .get("https://api.github.com/users/OldDognoob")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log("error");
+  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -31,10 +32,6 @@ axios.get('https://api.github.com/users/OldDognoob')
 */
 
 const followersArray = [];
-
-
-
-
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -64,59 +61,59 @@ const followersArray = [];
   bigknell
 */
 
-function cardUser(userinfo){
-   // 1- Create HTML markup
-  const card = document.createElement('div');
-  const cardimage = document.createElement('img');
-  const cardinfo = document.createElement('div');
-  const cardh3 = dosument.createElement('h3');
-  const carduserNamepara1=document.createElement('p');
-  const cardLocationpara2=document.createElement('p');
-  const cardProfilepara3=document.createElement('p');
-  const cardaddresshref=document.createElement('a');
-  const cardfollowerspara4=document.createElement('p');
-  const cardfollowingpara5=document.createElement('p');
-  const cardbiopara6=document.createElement('p');
+function cardUser(data) {
+  // 1- Create HTML markup
+  const parent = document.querySelector(".cards");
+
+  const card = document.createElement("div");
+  const cardimage = document.createElement("img");
+  const cardinfo = document.createElement("div");
+  const cardh3 = document.createElement("h3");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const a = document.createElement("a");
+  const p4 = document.createElement("p");
+  const p5 = document.createElement("p");
+  const p6 = document.createElement("p");
 
   // 2- Define HTML structure
-  card.appendChild(h3);
-  card.appendChild(userNamepara1);
-  card.appendChild(Locationpara2);
-  card.appendChild(Profilepara3);
-  card.appendChild(followerspara4);
-  card.appendChild(followingpara5);
-  card.appendChild(biopara6);
-  card.appendChild(addresshref);
+  parent.appendChild(card);
+  card.appendChild(cardinfo);
+  card.appendChild(cardimage);
+  cardinfo.appendChild(cardh3);
+  cardinfo.appendChild(p1);
+  cardinfo.appendChild(p2);
+  cardinfo.appendChild(p3);
+  cardinfo.appendChild(p4);
+  cardinfo.appendChild(p5);
+  cardinfo.appendChild(p6);
+  p3.appendChild(a);
 
   // 3- Add some class names
-  carddiv.classList.add('.card');
-  carddiv.classList.add('.card-info');
-  cardh3.classList.add('.name');
-  carduserNamepara1.classList.add('.username');
+  card.classList.add("card");
+  cardinfo.classList.add("card-info");
+  cardh3.classList.add("name");
+  p1.classList.add("username");
 
   // 4- Add some content!
-  h3.textContent=userInfo.name;
-  userNamepara1.textContent=userInfo.login;
-  locationpara2.textContent= userInfo.location;
-  profilepara3.textContent='Profile:';
-  anchor.textContent=userAddress.html_url;
-  followerspara4.textContent=`Followers: ${userInfo.followers}`;
-  followingpara5.textContent=`Following: ${userInfo.following}`;
-  biopara6.textContent=`Bio: ${userInfo.bio}`;
+  h3.textContent = data.name;
+  p1.textContent = data.login;
+  p2.textContent = data.location;
+  p3.textContent = `Profile: `;
+  a.textContent = userAddress.html_url;
+  p4.textContent = `Followers: ${userInfo.followers}`;
+  p5.textContent = `Following: ${userInfo.following}`;
+  p6.textContent = `Bio: ${userInfo.bio}`;
+
+  // 5- Add some attributes
+
 
 
   return card;
 }
+cardUser();
 
-const divcard = document.querySelector('.cards');
+const card = document.querySelector(".cards");
+
 // Making API Request
-axios.get('https://api.github.com/users/OldDognoob')
-.then(response =>{
- response.data.forEach(users => {
-   card.append.cardUser(users.data);
- });
-})
-.catch(error =>{
- console.log(error);
-});
-
